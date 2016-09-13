ee04bef83968c10d87e79f31bf00ce8c989ce7f5
Damith <dpremakumara@gmail.com>
2016-09-13 11:35:29 +0530

    change ticketUrl

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ var agentApp = angular.module('veeryAgentApp',
 agentApp.constant('moment', moment);
 
 var baseUrls = {
-    'authUrl':'http://userservice.app.veery.cloud/oauth/token',
-    'userServiceBaseUrl':'http://userservice.app.veery.cloud/DVP/API/1.0.0.0/',
+    'authUrl': 'http://userservice.app.veery.cloud/oauth/token',
+    'userServiceBaseUrl': 'http://userservice.app.veery.cloud/DVP/API/1.0.0.0/',
     'notification': 'notificationservice.app.veery.cloud',
     'ardsliteserviceUrl': 'http://ardsliteservice.app.veery.cloud/DVP/API/1.0.0.0/ARDS/resource',
-    'engagementUrl':'http://interactions.app.veery.cloud/DVP/API/1.0.0.0/',
-    'ticketUrl':'http://localhost:3636/DVP/API/1.0.0.0/',
-    'ivrUrl':'http://eventservice.app.veery.cloud/DVP/API/1.0.0.0/EventService/Events/SessionId/'
+    'engagementUrl': 'http://interactions.app.veery.cloud/DVP/API/1.0.0.0/',
+    'ticketUrl': 'http://192.168.5.186:3636/DVP/API/1.0.0.0/',
+    'ivrUrl': 'http://eventservice.app.veery.cloud/DVP/API/1.0.0.0/EventService/Events/SessionId/'
 };
 
 agentApp.constant('baseUrls', baseUrls);
