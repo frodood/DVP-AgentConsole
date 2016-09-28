55ef25abbb212a68ad0647c05b3f0739d8bc4031
heshanindika <k.heshan.indika@gmail.com>
2016-09-28 13:58:16 +0530

    add mising bower package

--- app/controller/console/consoleCtrl.js
+++ app/controller/console/consoleCtrl.js
@@ -853,6 +853,10 @@ agentApp.controller('consoleCtrl', function ($filter, $rootScope, $scope, $http,
 
 
     //----------------------SearchBar-----------------------------------------------------
+    //Main serch bar option
+
+    $scope.states = ["#ticket:", "#ticket:channel:", "#ticket:groupId:", "#ticket:tid:", "#ticket:priority:",
+        "#ticket:reference:", "#ticket:requester:", "#ticket:status:", "#profile:", "#profile:id:"];
 
     $scope.searchResult = [];
 
@@ -1265,16 +1269,6 @@ agentApp.controller('consoleCtrl', function ($filter, $rootScope, $scope, $http,
     getCurrentState.breakState();
     getCurrentState.getResourceState();
 
-    //Main serch bar option
-
-    $scope.states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
-        "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
-        "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
-        "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
-        "New York", "North Dakota", "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
-        "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
-        "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming",
-        "#ticket"];
 
 
 });
