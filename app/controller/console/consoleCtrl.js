0d1b1caaa8dcf5e397a39ba4b9351f28de73b9e1
Damith <dpremakumara@gmail.com>
2016-10-17 16:42:36 +0530

    update soft phone UI

--- app/controller/console/consoleCtrl.js
+++ app/controller/console/consoleCtrl.js
@@ -457,6 +457,7 @@ agentApp.controller('consoleCtrl', function ($filter, $rootScope, $scope, $http,
                 console.error(ex.message)
             }
         }
+
     };
 
     var phoneFuncion = {
@@ -2008,15 +2009,7 @@ agentApp.controller('consoleCtrl', function ($filter, $rootScope, $scope, $http,
         $('html, body').animate({scrollTop: 0}, 'fast');
     };
 
-    //code by damith
-    //check selected tab and focus
 
-    $('#consoleMainTab').find('li').on('click', function (ev) {
-        var $li = $(this).parent();
-
-        console.log('event fire..');
-
-    });
 
 
 }).directive("mainScroll", function ($window) {
--- assets/js/utility.js
+++ assets/js/utility.js
@@ -2,6 +2,7 @@
  * Created by Veery Team on 8/16/2016.
  */
 
+
 function getJSONData(http, file, callback) {
     http.get('assets/json/' + file + '.json').success(function (data) {
         callback(data.d);
@@ -51,5 +52,98 @@ $(document).ready(function () {
 $(window).resize(function () {
     resizeDiv();
 });
+
+
+//soft phone functions
+//code by damith
+//main console phone UI functions
+var softPhone = function () {
+    return {
+        muted: function () {
+        },
+        unmute: function () {
+        }
+    }
+}();
+
+var phoneAnimation = function () {
+    return {
+        autoHeightAnimate: function (element, time, height, callback) {
+            var curHeight = element.height(), // Get Default Height
+                autoHeight = element.css('height', 'auto').height(); // Get Auto Height
+            element.height(curHeight); // Reset to Default Height
+            element.stop().animate(
+                {
+                    height: height
+                }, {
+                    easing: 'swing',
+                    complete: function () {
+                        callback(true);
+                    }
+                }, time); // Animate to Auto Height
+        }
+    }
+}();
+
+function showMoreButton() {
+    $('#initiallyBtnWrapper').removeClass('display-block').addClass('display-none');
+    $('#phoneMoreBtnWrapper').removeClass('display-none').addClass('display-block');
+};
+function backToMaiaButtton() {
+    $('#initiallyBtnWrapper').removeClass('display-none').addClass('display-block');
+    $('#phoneMoreBtnWrapper').removeClass('display-block').addClass('display-none');
+};
+
+function callTransfer() {
+    $('#callTransfer').removeClass('display-none').addClass('display-block');
+    $('#phoneMoreBtnWrapper').removeClass('display-block').addClass('display-none');
+}
+
+function closeCallTransfer() {
+    $('#initiallyBtnWrapper').removeClass('display-none').addClass('display-block');
+    $('#callTransfer').removeClass('display-block').addClass('display-none');
+}
+
+function showDilapad() {
+    var $wrapper = $('.dial-pad-wrapper'),
+        animateTime = 500,
+        height = 310;
+    if ($wrapper.height() === 0) {
+        phoneAnimation.autoHeightAnimate($wrapper, animateTime, height, function (res) {
+            if (res) {
+                $('#phoneDialpad').removeClass('display-none').addClass('display-block');
+            }
+        });
+
+    } else {
+        $wrapper.stop().animate({height: '0'}, animateTime);
+        $('#phoneDialpad').removeClass('display-block').addClass('display-none');
+    }
+}
+
+function showMicrophoneOption() {
+    $('#microphoneOption').each(function (i, obj) {
+        var $i = $(this).parent();
+        if ($i.context) {
+            if ($i.context.children) {
+                for (var i = 0; i < $i.context.childNodes.length; i++) {
+                    var element = $i.context.childNodes[i];
+                    if (element != "#text") {
+                        if (element.id) {
+                            var id = ("#" + element.id);
+                            if ($(id).hasClass('display-block')) {
+                                $(id).removeClass('display-block').addClass('display-none');
+                            } else {
+                                $(id).removeClass('display-none').addClass('display-block');
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
+
 
 
