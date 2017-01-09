08e4f6f48cedcf4b7ee02b39a12147386988efd0
Pawan Sasanka <pawan@duosoftware.com>
2017-01-09 17:27:52 +0530

    Load tickets related to Groups completed/ Ticket assignee title added

--- app/controller/ticket/ticketService.js
+++ app/controller/ticket/ticketService.js
@@ -66,7 +66,7 @@ agentApp.factory("ticketService", function ($http, baseUrls, authService) {
         var authToken = authService.GetToken();
         return $http({
             method: 'GET',
-            url: baseUrls.ticketUrl + "Tickets/10/" + page + "?status=open&status=progressings"
+            url: baseUrls.ticketUrl + "Tickets/10/" + page + "?status=open&status=progressing"
         }).then(function (response) {
             return response;
         });
@@ -109,7 +109,7 @@ agentApp.factory("ticketService", function ($http, baseUrls, authService) {
         var authToken = authService.GetToken();
         return $http({
             method: 'GET',
-            url: baseUrls.ticketUrl + "MyTickets/10/" + page + "?status=open&status=progressings"
+            url: baseUrls.ticketUrl + "MyTickets/10/" + page + "?status=open&status=progressing"
         }).then(function (response) {
             return response;
         });
@@ -153,7 +153,7 @@ agentApp.factory("ticketService", function ($http, baseUrls, authService) {
         var authToken = authService.GetToken();
         return $http({
             method: 'GET',
-            url: baseUrls.ticketUrl + "MyGroupTickets/10/" + page + "?status=open&status=progressings"
+            url: baseUrls.ticketUrl + "MyGroupTickets/10/" + page + "?status=open&status=progressing"
         }).then(function (response) {
             return response;
         });
