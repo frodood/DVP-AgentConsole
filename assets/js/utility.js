4f1c2b0063407f2a9605b3eb1a8a41961650eef6
Damith <dpremakumara@gmail.com>
2017-01-24 16:45:57 +0530

    UI new update

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -196,15 +196,21 @@ window.onload = window.onresize = function () {
 // JavaScript
 var jsUpdateSize = function () {
     // Get the dimensions of the viewport
-    var width = window.innerWidth ||
-        document.documentElement.clientWidth ||
-        document.body.clientWidth;
+
     var height = window.innerHeight ||
         document.documentElement.clientHeight ||
         document.body.clientHeight;
     return height;
 };
 
+
+var jsUpdatewidth = function () {
+    // Get the dimensions of the viewport
+    var width = window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+    return width;
+};
 
 
 
