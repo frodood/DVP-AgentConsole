d242ef4929f2354f6b489df485109a7f3aa8b913
sukitha <sukitha@duosoftware.com>
2017-02-12 13:58:06 +0530

    url reset

--- app.js
+++ app.js
@@ -36,8 +36,7 @@ var baseUrls = {
     'integrationapi': 'http://localhost:4334/DVP/API/1.0.0.0/IntegrationAPI/',
     'sipuserUrl': 'http://sipuserendpointservice.app.veery.cloud/DVP/API/1.0.0.0/',
     'pwdVerifyUrl': 'http://userservice.app.veery.cloud/auth/verify',
-    'ipMessageURL': "http://127.0.0.1:3333",
-        //'http://ipmessagingservice.app.veery.cloud',
+    'ipMessageURL': 'http://ipmessagingservice.app.veery.cloud',
     'qaModule':'http://qamodule.app.veery.cloud/DVP/API/1.0.0.0/QAModule/'
 
 };
