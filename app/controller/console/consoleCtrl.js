9b7e3e17983c6384f230e8cbae94eceb9b78f419
Heshan Indika <k.heshan.indika@gmail.com>
2017-03-17 18:42:45 +0530

    Update consoleCtrl.js
    validate agent mode

--- app/controller/console/consoleCtrl.js
+++ app/controller/console/consoleCtrl.js
@@ -2922,11 +2922,14 @@ agentApp.controller('consoleCtrl', function ($filter, $rootScope, $scope, $http,
                             $('#Outbound').addClass('font-color-green bold');
                             $scope.currentModeOption = "Outbound";
                             return;
-                        } else {
+                        } else if (data.Result.Mode === "Inbound"){
                             $('#userStatus').addClass('online').removeClass('offline');
                             $('#Inbound').addClass('font-color-green bold');
                             $scope.currentModeOption = "Inbound";
                             return;
+                        } else{
+                          $('#userStatus').addClass('offline').removeClass('online');
+                            $scope.currentModeOption = "Offline";
                         }
                     }
                 }, function (error) {
